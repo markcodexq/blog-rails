115d6be06b03523b48a27ace3bc5e993be770294
markcodexq <markcodexq@gmail.com>
2022-05-30 13:31:28 +0500

    Add contacts controller and new.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
 Rails.application.routes.draw do
   get 'home/index'
 
+  resource :contacts, only: [:new, :create]
   resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
