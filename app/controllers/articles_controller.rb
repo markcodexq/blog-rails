62b133e6c55815a06bbe10df6c3341412710b570
markcodexq <markcodexq@gmail.com>
2022-06-01 19:16:15 +0500

    Implement articles#index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,13 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @articles = Article.all
+  end
 
   def show
     @article = Article.find(params[:id])
   end
-  def index
 
-  end
 
   def new
   end
