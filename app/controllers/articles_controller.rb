0bc8ff4b460d6b5616499b54ca1a563e3b7095b0
markcodexq <markcodexq@gmail.com>
2022-06-01 18:48:08 +0500

    implement articles show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
 
+
+  def show
+    @article = Article.find(params[:id])
+  end
   def index
 
   end
@@ -11,6 +15,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     if @article.valid?
       @article.save
+      redirect_to @article
     else
       render action: 'new'
     end
