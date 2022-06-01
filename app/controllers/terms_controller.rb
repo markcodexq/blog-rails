c551f05c6feb391a20a324642b0e383d87239da1
markcodexq <markcodexq@gmail.com>
2022-06-01 19:59:46 +0500

    add terms and about pages

--- app/controllers/terms_controller.rb
+++
@@ -1,5 +0,0 @@
-class TermsController < ApplicationController
-  def index
-
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
   get 'home/index'
-
+  get 'terms' => 'pages#terms'
+  get 'about' => 'pages#about'
   resource :contacts, only: [:new, :create]
   resources :articles
-  resources :about
-  resources :terms
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
