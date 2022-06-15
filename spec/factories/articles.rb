cf465a779809c2e5696efe3b72c2e0f2ae57512f
markcodexq <markcodexq@gmail.com>
2022-06-15 19:41:01 +0500

    update article factory

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,5 +2,15 @@ FactoryBot.define do
   factory :article do
     title { "Article title" }
     text { "Article text" }
+
+    # Создаём фарбику с названием article_with_comments
+    # Для создания статьи с несколькими комментариями
+    factory :article_with_comments do
+      # После создания article создаём список из комментариев
+      after :create do |article, evaluator|
+        create_list(:comment, 3, article: article)
+      end
+    end
+
   end
 end
