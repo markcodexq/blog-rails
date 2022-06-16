538d7dee669341f3e77805bf6de40e33f0ac6ae1
markcodexq <markcodexq@gmail.com>
2022-06-16 19:28:54 +0500

    update create contact test

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -4,6 +4,6 @@ feature "Contact Creations" do
   scenario "alows access to contact page" do
     visit "/contacts/new"
 
-    expect(page).to have_content 'Contact us'
+    expect(page).to have_content I18n.t("contacts.contact_us")
   end
 end
