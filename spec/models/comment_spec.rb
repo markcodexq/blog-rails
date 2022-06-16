2cb9bdecefe93f424cd41a553991af85bd54adf2
markcodexq <markcodexq@gmail.com>
2022-06-16 19:01:59 +0500

    update comment spec with validate body

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,4 +3,9 @@ require "rails_helper"
 
 describe Comment do
   it { should belong_to :article }
+
+  describe "validates comment length" do
+    it { should validate_length_of(:body).is_at_most(4000) }
+  end
+
 end
